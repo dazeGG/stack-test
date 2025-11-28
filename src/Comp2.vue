0a844a9fb41a73cce384a52e7ad3db8892ffa808
VladimirBelyaev <vogisa@mail.ru>
2025-11-28 21:50:57 +0300

    feat: added text prop and attr to advanced testing

--- src/Comp2.vue
+++ src/Comp2.vue
@@ -3,11 +3,17 @@ defineProps({
 	disabled: {
 		type: Boolean,
 		default: false
+	},
+	text: {
+		type: String,
+		default: 'zxczxc'
 	}
 })
 </script>
 
 <template>
+	{{ text }}
+	<hr>
 	{{ disabled }} <!-- Отображается: true  -->
 	<button :disabled="disabled">Кнопка</button> <!-- Заблокирована  -->
 </template>
